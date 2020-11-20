209556c482d8220a98d4dfd3ae1b31f024a8dba4
Patrick Ng <corgis@Patricks-MacBook-Pro.local>
2020-11-19 20:09:55 -0800

    implemented tests

--- client/src/components/Modals/Checkout/Right.jsx
+++ client/src/components/Modals/Checkout/Right.jsx
@@ -1,126 +1,128 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Right = (props) => {
-  const Right = styled.div`
-    display: flex;
-    padding: 10px;
-    flex-wrap: wrap;
-  `
+const Container = styled.div`
+display: flex;
+padding: 10px;
+flex-wrap: wrap;
+`
 
-  const Title = styled.div`
-    height: 10%;
-    width: 100%;
-    font-family: "AdihausDIN";
-  `
+const Title = styled.div`
+height: 10%;
+width: 100%;
+font-family: "AdihausDIN";
+`
 
-  const ItemCount = styled.div`
-    margin: 20px 0px 10px 0px;
-    height: 10%;
-    width: 100%;
-    font-family: "AdihausDIN";
-  `
-  const TotalCost = styled.div`
-    display: flex;
-    height: 10%;
-    width: 100%;
-    justify-content: space-between;
-    font-family: "AdihausDIN";
-  `
-  const TotalText = styled.div`
-    height: 10%;
-    width: 40%;
-    font-family: "AdihausDIN";
-  `
-  const TotalAmount = styled.div`
-    height: 10%;
-    width: 20%;
-    font-family: "AdihausDIN";
-  `
-  const TotalDelivery = styled.div`
-    display: flex;
-    height: 10%;
-    width: 100%;
-    justify-content: space-between;
-    border-bottom: 1px solid black;
-    font-family: "AdihausDIN";
-  `
+const ItemCount = styled.div`
+margin: 20px 0px 10px 0px;
+height: 10%;
+width: 100%;
+font-family: "AdihausDIN";
+`
+const TotalCost = styled.div`
+display: flex;
+height: 10%;
+width: 100%;
+justify-content: space-between;
+font-family: "AdihausDIN";
+`
+const TotalText = styled.div`
+height: 10%;
+width: 40%;
+font-family: "AdihausDIN";
+`
+const TotalAmount = styled.div`
+height: 10%;
+width: 20%;
+font-family: "AdihausDIN";
+`
+const TotalDelivery = styled.div`
+display: flex;
+height: 10%;
+width: 100%;
+justify-content: space-between;
+border-bottom: 1px solid black;
+font-family: "AdihausDIN";
+`
 
-  const TotalDeliveryText = styled.div`
-    height: 10%;
-    width: 40%;
-    font-family: "AdihausDIN";
-  `
+const TotalDeliveryText = styled.div`
+height: 10%;
+width: 40%;
+font-family: "AdihausDIN";
+`
 
-  const TotalDeliveryAmount = styled.div`
-    height: 10%;
-    width: 20%;
-    font-family: "AdihausDIN";
-  `
+const TotalDeliveryAmount = styled.div`
+height: 10%;
+width: 20%;
+font-family: "AdihausDIN";
+`
 
-  const Lower = styled.div`
-    height: 49%;
-    width: 100%;
-  `
+const Lower = styled.div`
+height: 49%;
+width: 100%;
+`
 
-  const TotalRow = styled.div`
-    display: flex;
-    height: 10%;
-    width: 100%;
-    justify-content: space-between;
-    margin-top: 5px;
-  `
+const TotalRow = styled.div`
+display: flex;
+height: 10%;
+width: 100%;
+justify-content: space-between;
+margin-top: 5px;
+`
 
-  const Total = styled.div`
-    height: 10%;
-    width: 40%;
-    font-family: 'adineue PRO KZ Bold';
-  `
-  const PriceSum = styled.div`
-    height: 10%;
-    width: 20%;
-    font-family: 'adineue PRO KZ Bold';
-  `
-  const CheckoutButton = styled.button`
-    margin-top: 2%;
-    height: 50px;
-    width: 300px;
-    color: black;
-    background-color: white;
-    justify-content: space-between;
-    font-family: "AdihausDIN";
-    font-size: 18px;
-    position: relative;
-    cursor: pointer;
-  `
-  const ViewBagButton = styled.button`
-    margin-top: 2%;
-    height: 50px;
-    width: 300px;
-    color: white;
-    background-color: black;
-    justify-content: space-between;
-    font-family: "AdihausDIN";
-    font-size: 18px;
-    position: relative;
-    cursor: pointer;
-  `
-  const AddButton = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+const Total = styled.div`
+height: 10%;
+width: 40%;
+font-family: 'adineue PRO KZ Bold';
+`
+const PriceSum = styled.div`
+height: 10%;
+width: 20%;
+font-family: 'adineue PRO KZ Bold';
+`
+const CheckoutButton = styled.button`
+margin-top: 2%;
+height: 50px;
+width: 300px;
+color: black;
+background-color: white;
+justify-content: space-between;
+font-family: "AdihausDIN";
+font-size: 18px;
+position: relative;
+cursor: pointer;
+`
+const ViewBagButton = styled.button`
+margin-top: 2%;
+height: 50px;
+width: 300px;
+color: white;
+background-color: black;
+justify-content: space-between;
+font-family: "AdihausDIN";
+font-size: 18px;
+position: relative;
+cursor: pointer;
+`
+const AddButton = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
 `
 const Add = styled.div`
-  margin-left: 5%;
+margin-left: 5%;
 `
 const Arrow = styled.div`
-  font-size: 20px;
-  margin-right: 5%;
+font-size: 20px;
+margin-right: 5%;
 `
 
+
+const Right = (props) => {
   const price = props.quantity * 140;
+
   return (
-    <Right>
+    <Container>
         <Title>Your Bag</Title>
         <ItemCount>{props.quantity} items</ItemCount>
         <TotalCost>
@@ -137,7 +139,7 @@ const Arrow = styled.div`
         </TotalRow>
         <ViewBagButton><AddButton><Add>View Bag</Add> <Arrow>→</Arrow></AddButton></ViewBagButton>
         <CheckoutButton><AddButton><Add>Check out</Add> <Arrow>→</Arrow></AddButton></CheckoutButton>
-    </Right>
+    </Container>
   )
 }
 
