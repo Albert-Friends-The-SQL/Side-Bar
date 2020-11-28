ac6bcb2faf92e67df623e7e113248b28d607f511
Patrick Ng <corgis@Patricks-MacBook-Pro.local>
2020-11-28 12:08:09 -0800

    made css changes

--- client/src/components/Modals/SizeGuide/Modal.jsx
+++ client/src/components/Modals/SizeGuide/Modal.jsx
@@ -8,92 +8,34 @@ import data2 from '../../../../../cm.json';
 
 const Modal = ({ open, onClose }) => {
   if (!open) return null;
-  const Container = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    /* justify-content: center; */
-    flex-direction: column;
-    background-color: #fefefe;
-    margin: 12% auto; /* 15% from the top and centered */
-    padding: 10px;
-    border: 1px solid black;
-    width: 50%; /* Could be more or less, depending on screen size */
-    height: 85%;
-    position: relative;
-    transition: visibility 1s, opacity 0.5s;
-  `
+
   const [isClicked, setIsClicked] = useState(true);
 
-  const Overlay = styled.div`
-    display: flex;
-    position: fixed;
-    z-index: 10000;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100vh;
-    background-color: rgba(0,0,0,0.5);
-    justify-content: center;
-    align-items: center;
-  `
-  const Header = styled.div`
-    margin-top: 5%;
-    margin-left: 1%;
-    font-size: 28px;
-    height: 8%;
-    font-family: 'adineue PRO KZ Bold';
-  `
-  const Exit = styled.button`
-      position: absolute;
-      height: 40px;
-      width: 40px;
-      background-color: white;
-      color: black;
-      cursor: pointer;
-      border: 1px solid black;
-      top: 5px;
-      right: 5px;
-      font-size: 32px;
-      font-family: 'Arial';
-  `
-  const Chart = styled.div`
-    display: flex;
-    flex-direction: column;
-    overflow-x: auto;
-    justify-content: center;
-    width: 100%;
-  `
   const Inches = styled.div`
-    display: flex;
-    height: 50%;
-    width: 50%;
-    justify-content: center;
-    align-items: center;
-    border: 1px solid black;
-    margin-bottom: 10px;
-    cursor: pointer;
-    border-bottom: ${isClicked ? '2px solid black' : '1px solid black'};
-    color: ${isClicked ? 'black' : 'grey'};
-    font-family: 'AdihausDIN';
+  display: flex;
+  height: 50%;
+  width: 50%;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid black;
+  margin-bottom: 10px;
+  cursor: pointer;
+  border-bottom: ${isClicked ? '2px solid black' : '1px solid black'};
+  color: ${isClicked ? 'black' : 'grey'};
+  font-family: 'AdihausDIN';
   `
-   const Cm = styled.div`
-   display: flex;
-   height: 50%;
-   width: 50%;
-   justify-content: center;
-   align-items: center;
-   border: 1px solid black;
-   margin-bottom: 10px;
-   cursor: pointer;
-   border-bottom: ${isClicked ? '1px solid black' :'2px solid black'};
-   color: ${isClicked ? 'grey' : 'black'};
-   font-family: 'AdihausDIN';
- `
-  const Buttons = styled.div`
-    display:flex;
-    flex-direction: row;
-    width: 26%;
-    height: 12%;
+  const Cm = styled.div`
+  display: flex;
+  height: 50%;
+  width: 50%;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid black;
+  margin-bottom: 10px;
+  cursor: pointer;
+  border-bottom: ${isClicked ? '1px solid black' :'2px solid black'};
+  color: ${isClicked ? 'grey' : 'black'};
+  font-family: 'AdihausDIN';
   `
 
 
@@ -141,11 +83,6 @@ const Modal = ({ open, onClose }) => {
             <Inches onClick={()=> setIsClicked(true)}>Inches</Inches>
             <Cm onClick={()=> setIsClicked(false)}>cm</Cm>
           </Buttons>
-          {/* <Chart>
-            {data.map((category, index) => {
-              return <Table category={category} key={index} />
-            })}
-          </Chart> */}
           {showTable()}
           <Fit>Scroll horizontally to see more.</Fit>
           <Question>In Between Sizes?</Question>
@@ -160,4 +97,65 @@ const Modal = ({ open, onClose }) => {
 
 export default Modal;
 
-// font-family: 'adineue PRO KZ Bold';
+
+const Container = styled.div`
+display: flex;
+flex-wrap: wrap;
+/* justify-content: center; */
+flex-direction: column;
+background-color: #fefefe;
+margin: 12% auto; /* 15% from the top and centered */
+padding: 10px;
+border: 1px solid black;
+width: 50%; /* Could be more or less, depending on screen size */
+height: 85%;
+position: relative;
+transition: visibility 1s, opacity 0.5s;
+`
+
+const Overlay = styled.div`
+display: flex;
+position: fixed;
+z-index: 10000;
+left: 0;
+top: 0;
+width: 100%;
+height: 100vh;
+background-color: rgba(0,0,0,0.5);
+justify-content: center;
+align-items: center;
+`
+const Header = styled.div`
+margin-top: 5%;
+margin-left: 1%;
+font-size: 28px;
+height: 8%;
+font-family: 'adineue PRO KZ Bold';
+`
+const Exit = styled.button`
+  position: absolute;
+  height: 40px;
+  width: 40px;
+  background-color: white;
+  color: black;
+  cursor: pointer;
+  border: 1px solid black;
+  top: 5px;
+  right: 5px;
+  font-size: 32px;
+  font-family: 'Arial';
+`
+const Chart = styled.div`
+display: flex;
+flex-direction: column;
+overflow-x: auto;
+justify-content: center;
+width: 100%;
+`
+
+const Buttons = styled.div`
+display:flex;
+flex-direction: row;
+width: 26%;
+height: 12%;
+`
