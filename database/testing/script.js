5f4ee0692e4671aeab1dc87b1c1ce2c93369c0ec
Jason Chen <jason.l.chen@gmail.com>
2020-12-08 16:53:13 -0800

    setup CREATE route and test endpoints

--- database/testing/script.js
+++ database/testing/script.js
@@ -1,5 +1,5 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { sleep, group } from 'k6';
 import faker from 'https://cdnjs.cloudflare.com/ajax/libs/Faker/3.1.0/faker.min.js';
 
 export let options = {
@@ -11,9 +11,16 @@ export default function () {
   const randomVariantId = faker.random.number({ min: 1, max: 25000000});
 
   // SINGLE QUERY
+  group('Single Query', () => {
+    http.get(`http://localhost:3000/api/tests/single_query/${randomVariantId}`);
+  });
   // http.get(`http://localhost:3000/api/tests/single_query/${randomVariantId}`);
 
   //MULTI QUERY
-  http.get(`http://localhost:3000/api/tests/multi_query1/${randomVariantId}`);
-  http.get(`http://localhost:3000/api/tests/multi_query2/${randomVariantId}`);
+  group('Multi Query', () => {
+    http.get(`http://localhost:3000/api/tests/multi_query1/${randomVariantId}`);
+     http.get(`http://localhost:3000/api/tests/multi_query2/${randomVariantId}`);
+  });
+
+
 }
